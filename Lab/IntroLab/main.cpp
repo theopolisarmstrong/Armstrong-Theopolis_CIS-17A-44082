9bccb90274003212b1b8f4034017036ab2e3f022
Trey Armstrong <theopolisarmstrong@gmail.com>
2019-02-12 15:15:59 -0800

    IntroLab Completed Signed-off-by: Trey Armstrong <theopolisarmstrong@gmail.com>

---
+++ Lab/IntroLab/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Theopolis Armstrong
+ * Created on February 12th, 2019, 3:09 PM
+ * Purpose:  IntroLab "Hello World" program
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants, no Global Variables are allowed
+//Math/Physics/Conversions/Higher Dimensions - i.e. PI, e, etc...
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Set the random number seed
+    
+    //Declare Variables
+    
+    //Initialize or input i.e. set variable values
+    
+    //Map inputs -> outputs
+    
+    //Display the outputs
+    cout << "Hello, World!\n";
+    
+    //Exit stage right or left!
+    return 0;
+}
