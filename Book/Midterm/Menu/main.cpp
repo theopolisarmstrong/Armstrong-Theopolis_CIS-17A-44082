576428ad61cdbb85118155b787d69f05b37b7c00
Trey Armstrong <theopolisarmstrong@gmail.com>
2019-04-17 10:28:29 -0700

    Signed-off-by: Trey Armstrong <theopolisarmstrong@gmail.com>

--- Book/Midterm/Menu/main.cpp
+++ Book/Midterm/Menu/main.cpp
@@ -7,6 +7,7 @@
 
 //System Libraries
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 //User Libraries
@@ -14,12 +15,26 @@ using namespace std;
 //Global Constants - Math/Physics Constants, Conversions,
 //                   2-D Array Dimensions
 
+//Structures
+struct Checking{
+    string name; //Account holder name
+    string address; //Account holder address
+    unsigned int number; //Account number (5 digits)
+    float balance; //Account balance
+    float totCheck; //Total monthly check withdrawals
+    float totDep; //Total deposits
+};
+
 //Function Prototypes
 void menu();
 void prblm1();
 void prblm2();
 void prblm3();
 void prblm4();
+//Problem 1 functions
+void valid(float &input, string error);
+float inLoop(const float);
+//Problem 2 functions
 
 //Execution Begins Here
 int main(int argc, char** argv) {
@@ -55,8 +70,8 @@ int main(int argc, char** argv) {
 void menu(){
     //Display menu
     cout<<endl<<"Choose from the following Menu"<<endl;
-    cout<<"Type 1 for Independent-If example"<<endl;
-    cout<<"Type 2 for Dependent-If example"<<endl;
+    cout<<"Type 1 for Checking Account Balance program (Problem 1)"<<endl;
+    cout<<"Type 2 for Gross Employee Pay program (Problem 2)"<<endl;
     cout<<"Type 3 for Ternary Op example"<<endl;   
     cout<<"Type 4 for Switch-Case example"<<endl<<endl;
 }
@@ -69,7 +84,63 @@ void menu(){
 //000000011111111112222222222333333333344444444445555555555666666666677777777778
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
 void prblm1(){
+    cin.ignore(); //Ignore menu choice input
     cout<<"Problem 1"<<endl;
+    //Set the random number seed
+    
+    //Declare Variables
+    const float ODF  = 20; //Overdraft fee
+    
+    Checking* account = new Checking;
+    float input, odBal=0; //Loop input; estimated overdraft balance
+    
+    //Initialize or input i.e. set variable values
+    cout << "Enter checking account information.\n";
+    cout << "Name: ";
+    getline(cin, account->name);
+    cout << "Address: ";
+    getline(cin, account->address);
+    cout << "Account number (5 digits): ";
+    cin >> input;
+    valid(input, "Error: account number out of range. Enter a 5 digit account number: ");
+    account->number = input;
+    cout << "Starting Balance: ";
+    cin >> account->balance;
+    cout << "Value of each check written this month (Enter a 0 to continue): \n";
+    account->totCheck = inLoop(0);
+    cout << "Value of each deposit this month (Enter a 0 to continue): \n";
+    account->totDep = inLoop(0);
+    
+    //Map inputs -> outputs
+    account->balance += (account->totDep - account->totCheck);
+    if(account->balance < 0){
+        odBal = account->balance - ODF;
+    }
+    
+    //Display the outputs
+    cout << setw(16) << left << "Account name: " << account->name << endl;
+    cout << setw(16) << left << "Address: " << account->address << endl;
+    cout << setw(16) << left << "Account number: " << setfill('0') << setw(5) << account->number << endl;
+    cout << setprecision(2) << fixed;
+    cout << setw(16) << left << "Total balance : $" << account->balance << endl;
+    if(odBal) cout << "\nYou will be charged an overdraft fee of $" << ODF
+                   << ".\nYour new estimated balance is $" << odBal << ".\n";
+}
+void valid(float &input, string error){
+    while (input > 99999){
+        cout << error << endl;
+        cin >> input;
+    }
+}
+float inLoop(const float exit){
+    float input, total = 0;
+    
+    cin >> input;
+    while(input != exit){
+        total += input;
+        cin >> input;
+    }
+    return total;
 }
 
 //000000011111111112222222222333333333344444444445555555555666666666677777777778
--- Book/Midterm/Midterm_Prob1_CheckingAccountBalance/main.cpp
+++ Book/Midterm/Midterm_Prob1_CheckingAccountBalance/main.cpp
@@ -7,6 +7,7 @@
 
 //System Libraries
 #include <iostream>  //Input/Output Library
+#include <iomanip> //Input/Output Manipulation
 using namespace std;
 
 //User Libraries
@@ -16,28 +17,79 @@ using namespace std;
 
 //Structures
 struct Checking{
-    string name;
-    string address;
-    unsigned int number;
-    int balance;
-    int totCheck;
-    int totDep;
+    string name; //Account holder name
+    string address; //Account holder address
+    unsigned int number; //Account number (5 digits)
+    float balance; //Account balance
+    float totCheck; //Total monthly check withdrawals
+    float totDep; //Total deposits
 };
 
 //Function Prototypes
+void valid(float &input, string error);
+float inLoop(const float);
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Set the random number seed
     
     //Declare Variables
+    const float ODF  = 20; //Overdraft fee
+    
+    Checking* account = new Checking;
+    float input, odBal=0; //Loop input; estimated overdraft balance
     
     //Initialize or input i.e. set variable values
+    cout << "Enter checking account information.\n";
+    cout << "Name: ";
+    getline(cin, account->name);
+    cout << "Address: ";
+    getline(cin, account->address);
+    cout << "Account number (5 digits): ";
+    cin >> input;
+    valid(input, "Error: account number out of range. Enter a 5 digit account number: ");
+    account->number = input;
+    cout << "Starting Balance: ";
+    cin >> account->balance;
+    cout << "Value of each check written this month (Enter a 0 to continue): \n";
+    account->totCheck = inLoop(0);
+    cout << "Value of each deposit this month (Enter a 0 to continue): \n";
+    account->totDep = inLoop(0);
     
     //Map inputs -> outputs
+    account->balance += (account->totDep - account->totCheck);
+    if(account->balance < 0){
+        odBal = account->balance - ODF;
+    }
     
     //Display the outputs
+    cout << setw(16) << left << "Account name: " << account->name << endl;
+    cout << setw(16) << left << "Address: " << account->address << endl;
+    cout << setw(16) << left << "Account number: " << setfill('0') << setw(5) << account->number << endl;
+    cout << setprecision(2) << fixed;
+    cout << setw(16) << left << "Total balance : $" << account->balance << endl;
+    if(odBal) cout << "\nYou will be charged an overdraft fee of $" << ODF
+                   << ".\nYour new estimated balance is $" << odBal << ".\n";
+    
 
     //Exit stage right or left!
     return 0;
+}
+
+void valid(float &input, string error){
+    while (input > 99999){
+        cout << error << endl;
+        cin >> input;
+    }
+}
+
+float inLoop(const float exit){
+    float input, total = 0;
+    
+    cin >> input;
+    while(input != exit){
+        total += input;
+        cin >> input;
+    }
+    return total;
 }
