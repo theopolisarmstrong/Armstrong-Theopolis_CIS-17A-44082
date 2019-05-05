07f7a3e578d5dbbf65489ba00549ed895e59ee00
Trey Armstrong <theopolisarmstrong@protonmail.com>
2019-05-05 06:05:59 -0700

    Homework Assignment 4 complete

--- Xcode/Assignment_4/Gaddis_9thEd_Chap13_Prob6/Gaddis_9thEd_Chap13_Prob6/Inventory.h
+++ Xcode/Assignment_4/Gaddis_9thEd_Chap13_Prob6/Gaddis_9thEd_Chap13_Prob6/Inventory.h
@@ -3,25 +3,6 @@
  * Author: Theopolis Armstrong
  * Created on May 5th, 2019, 5:12 AM
  * Purpose: Inventory class declaration
- * ---------------------------------------------------
- * |                  Inventory                      |
- * |-------------------------------------------------|
- * | - itemNumber : int                              |
- * | - quantity : int                                |
- * | - cost : float                                  |
- * | - totalCost : float                             |
- * |-------------------------------------------------|
- * | + Inventory() :                                 |
- * | + Inventory(id : int, c : float, quant : int) : |
- * | + setItemNumber(id : int) : void                |
- * | + setQuantity(quant : int) : void               |
- * | + setCost(c : float) : void                     |
- * | + setTotalCost() : void                         |
- * | + getItemNumber() : int                         |
- * | + getQuantity() : int                           |
- * | + getCost() : float                             |
- * | + getTotalCost() : float                        |
- * ---------------------------------------------------
  */
 
 #ifndef Inventory_h
@@ -33,7 +14,19 @@ using namespace std;
 class Inventory{
 private:
     int itemNumber;
-    int item
+    int quantity;
+    float cost;
+    float totalCost;
+public:
+    Inventory() : itemNumber(0), quantity(0), cost(0.0), totalCost(0.0) {}
+    Inventory(int num, int quant, float c) {itemNumber = num; quantity = quant; cost = c; }
+    void setItemNumber(int num) { itemNumber = num; }
+    void setQuantity(int quant) { quantity = quant; }
+    void setCost(double c) { cost = c; }
+    int getItemNumber() { return itemNumber; }
+    int getQuantity() { return quantity; }
+    float getCost() { return cost; }
+    float getTotalCost() { return quantity * cost; }
 };
 
 #endif /* Inventory_h */
