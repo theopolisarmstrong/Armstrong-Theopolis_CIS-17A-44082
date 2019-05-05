61352abdf96f88bb8cf5c2bd57276e8775bb1afd
Trey Armstrong <theopolisarmstrong@protonmail.com>
2019-05-05 05:29:07 -0700

    Homework Assignment 4

---
+++ Xcode/Assignment_4/Gaddis_9thEd_Chap13_Prob6/Gaddis_9thEd_Chap13_Prob6/Inventory.h
@@ -0,0 +1,39 @@
+/*
+ * File:   main.cpp
+ * Author: Theopolis Armstrong
+ * Created on May 5th, 2019, 5:12 AM
+ * Purpose: Inventory class declaration
+ * ---------------------------------------------------
+ * |                  Inventory                      |
+ * |-------------------------------------------------|
+ * | - itemNumber : int                              |
+ * | - quantity : int                                |
+ * | - cost : float                                  |
+ * | - totalCost : float                             |
+ * |-------------------------------------------------|
+ * | + Inventory() :                                 |
+ * | + Inventory(id : int, c : float, quant : int) : |
+ * | + setItemNumber(id : int) : void                |
+ * | + setQuantity(quant : int) : void               |
+ * | + setCost(c : float) : void                     |
+ * | + setTotalCost() : void                         |
+ * | + getItemNumber() : int                         |
+ * | + getQuantity() : int                           |
+ * | + getCost() : float                             |
+ * | + getTotalCost() : float                        |
+ * ---------------------------------------------------
+ */
+
+#ifndef Inventory_h
+#define Inventory_h
+
+#include <iostream>
+using namespace std;
+
+class Inventory{
+private:
+    int itemNumber;
+    int item
+};
+
+#endif /* Inventory_h */
---
+++ Xcode/Assignment_4/Gaddis_9thEd_Chap13_Prob6/Gaddis_9thEd_Chap13_Prob6/main.cpp
@@ -0,0 +1,52 @@
+/*
+ * File:   main.cpp
+ * Author: Theopolis Armstrong
+ * Created on May 5th, 2019, 5:12 AM
+ * Purpose: Create and demonstrate an inventory class using the following UML class diagram
+ * ---------------------------------------------------
+ * |                  Inventory                      |
+ * |-------------------------------------------------|
+ * | - itemNumber : int                              |
+ * | - quantity : int                                |
+ * | - cost : float                                  |
+ * | - totalCost : float                             |
+ * |-------------------------------------------------|
+ * | + Inventory() :                                 |
+ * | + Inventory(id : int, c : float, quant : int) : |
+ * | + setItemNumber(id : int) : void                |
+ * | + setQuantity(quant : int) : void               |
+ * | + setCost(c : float) : void                     |
+ * | + setTotalCost() : void                         |
+ * | + getItemNumber() : int                         |
+ * | + getQuantity() : int                           |
+ * | + getCost() : float                             |
+ * | + getTotalCost() : float                        |
+ * ---------------------------------------------------
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants, no Global Variables are allowed
+//Math/Physics/Conversions/Higher Dimensions - i.e. PI, e, etc...
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Set the random number seed
+    
+    //Declare Variables
+    
+    //Initialize or input i.e. set variable values
+    
+    //Map inputs -> outputs
+    
+    //Display the outputs
+    
+    //Exit stage right or left!
+    return 0;
+}
